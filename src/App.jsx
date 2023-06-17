88767703bb7a041c43a3e31715d822023cccd786
NEAR97 <instantcrushxx97@gmail.com>
2023-06-17 13:33:02 -0600

    header creado y estilos agregados

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-red-400'>
-       Hola MUNDO
+      <div className='min-h-screen bg-[#181A20]'>
+       <Header/>
+       <main  className='h-[90vh] flex'>
+          <div>
+            Sidebar
+          </div>
+          <div>
+            content
+          </div> 
+
+       </main>
       </div>
      
     </>
