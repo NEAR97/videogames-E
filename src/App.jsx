76842ee21030d45ac06ac51edcfbb166adde6a07
NEAR97 <instantcrushxx97@gmail.com>
2023-06-17 18:36:32 -0600

    sidebar creado

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='min-h-screen bg-[#181A20]'>
+      <div className='min-h-screen bg-[#181A20] p-8'>
        <Header/>
-       <main  className='h-[90vh] flex'>
-          <div>
-            Sidebar
-          </div>
-          <div>
+       <main  className='h-[90vh] flex p-4'>
+          
+          <Sidebar/>
+          
+          <div className='flex-1 bg-blue-200 h-full overflow-y-scroll'>
             content
           </div> 
 
