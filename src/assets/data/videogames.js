5a35a3a37c804900c0fe8fa8126df64d59a74ec8
NEAR97 <instantcrushxx97@gmail.com>
2023-06-19 22:57:39 -0600

    build creado

--- src/assets/data/videogames.js
+++ src/assets/data/videogames.js
@@ -22,7 +22,7 @@ export const videogames = [
     'name': 'Little Nightmares II',
     'type': 'Indie',
     'console': 'PS4',
-    'description': 'brother',
+    'description': 'Little Nightmares II es un juego de aventura de suspense en el que juegas como Mono, un joven chico atrapado en un mundo que ha sido tergiversado por la resonante transmisión de una lejana torre.',
     'price': 19.99,
     'image': 'https://image.api.playstation.com/vulcan/ap/rnd/202204/1908/Z1D1p88W4XwXXXgyGyJ9SK34.jpg'
   },
@@ -31,7 +31,7 @@ export const videogames = [
     'name': 'Super Smash Bros',
     'type': 'Casual',
     'console': 'Switch',
-    'description': 'Battle',
+    'description': '¡Un auténtico duelo de titanes de los videojuegos que podrás jugar cuando quieras y donde quieras! Lanza a tus rivales del escenario usando uno de los nuevos personajes como Simon Belmont o King K. Rool que se unen a Inkling, Ridley y a todos los combatientes presentes en la historia de Super Smash Bro',
     'price': 29.99,
     'image': 'https://m.media-amazon.com/images/I/81qXIz8es3L.jpg'
   },
@@ -40,7 +40,7 @@ export const videogames = [
     'name': 'Cuphead',
     'type': 'Adventure',
     'console': 'Switch',
-    'description': 'Platform',
+    'description': 'Cuphead es un juego de acción clásico estilo "dispara y corre" que se centra en combates contra el jefe. Inspirado en los dibujos animados de los años 30, los aspectos visual y sonoro están diseñados con esmero empleando las mismas técnicas de la época, es decir, animación tradicional a mano, fondos de acuarela y grabaciones originales de jazz.',
     'price': 9.99,
     'image': 'https://uvejuegos.com/img/caratulas/62516/Cuphead-switch.jpg'
   },
@@ -49,7 +49,7 @@ export const videogames = [
     'name': 'Modern Warfare 2',
     'type': 'Shooter',
     'console': 'Xbox',
-    'description': 'Modern Warfare 2',
+    'description': 'Call of Duty: Modern Warfare II es un videojuego de disparos en primera persona desarrollado por Infinity Ward y publicado por Activision, lanzandose para PlayStation 4, PlayStation 5, Xbox One, Xbox Series X y Series S y Microsoft Windows.​​',
     'price': 49.99,
     'image': 'https://i0.wp.com/news.xbox.com/es-latam/wp-content/uploads/sites/4/Untitled-21-82e903a3bf71be3acd94.png?fit=1200%2C675&ssl=1'
   },
@@ -58,7 +58,7 @@ export const videogames = [
     'name': 'FIFA 23',
     'type': 'Sport',
     'console': 'PS5',
-    'description': 'Fifa version 23',
+    'description': 'Juega en cuatro estadios auténticos con los 12 equipos de la NWSL en varios modos de juego, como Patada inicial clásica y Temporadas cooperativas. ',
     'price': 19.99,
     'image': 'https://image.api.playstation.com/vulcan/ap/rnd/202301/0312/GU1vXFJpbzGYNV6UN3U0Cnnb.png'
   },
@@ -67,7 +67,7 @@ export const videogames = [
     'name': 'Halo Infinite',
     'type': 'Shooter',
     'console': 'Xbox',
-    'description': 'Halo Infinite',
+    'description': 'La legendaria saga Halo regresa con la campaña de Master Chief más amplia hasta la fecha y una experiencia multijugador gratuita revolucionaria.',
     'price': 19.99,
     'image': 'https://www.somosxbox.com/wp-content/uploads/2020/07/portada-oficial-de-Halo-Infinite.jpg'
   },
@@ -76,7 +76,7 @@ export const videogames = [
     'name': 'Fall Guys',
     'type': 'Adventure',
     'console': 'PC',
-    'description': 'Fall Guys',
+    'description': 'Fall Guys es un party royale gratuito, multiplataforma y multijugador masivo en el que tú y tus contrincantes compiten en caóticas rondas repletas de obstáculos absurdos hasta que solo quede un ganador.',
     'price': 9.99,
     'image': 'https://cdn1.epicgames.com/offer/50118b7f954e450f8823df1614b24e80/EGS_FallGuys_Mediatonic_S2_1200x1600-f8dd257a8537df053a64c4e5063e62ea'
   },
@@ -85,7 +85,7 @@ export const videogames = [
     'name': 'Outlast Trials',
     'type': 'Adventure',
     'console': 'PC',
-    'description': 'Outlast',
+    'description': 'Red Barrels invites you to experience mind-numbing terror, this time with friends. Whether you go through the trials alone or in teams, if you survive long enough and complete the therapy, Murkoff will happily let you leave… but will you be the same?',
     'price': 35.99,
     'image': 'https://cdn1.epicgames.com/6504cc61472e498796e0b4963a201438/offer/EGS_TheOutlastTrials_RedBarrels_S2-1200x1600-c70676de7925195513fa7bc4bc2f1d9b.jpg'
   },
@@ -94,7 +94,7 @@ export const videogames = [
     'name': 'Resident Evil 4',
     'type': 'Adventure',
     'console': 'PS5',
-    'description': 'Resident Evil 4',
+    'description': 'Resident Evil 4 conserva la esencia del juego original, a la vez que introduce mecánicas de juego actualizadas,una historia reimaginada, e impresionantes gráficos de última generación que lo convierten en la experiencia de Survival Horror definitiva en la cual se cruzan la vida y la muerte, el terror y la catarsis.',
     'price': 49.99,
     'image': 'https://image.api.playstation.com/vulcan/ap/rnd/202210/0706/EVWyZD63pahuh95eKloFaJuC.png'
   },
@@ -103,7 +103,7 @@ export const videogames = [
     'name': 'Ghost of Tsushima',
     'type': 'Adventure',
     'console': 'PS4',
-    'description': 'Ninjas',
+    'description': 'Descubre las maravillas ocultas de Tsushima en esta aventura de acción en un mundo abierto creada por Sucker Punch Productions y PlayStation Studios, disponible en PS5 y PS4.',
     'price': 29.99,
     'image': 'https://image.api.playstation.com/vulcan/ap/rnd/202010/0222/niMUubpU9y1PxNvYmDfb8QFD.png'
   },
